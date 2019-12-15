68d75c999bb3358f431f63fbfbfcb1efc5dd6046
Guillem LLEIDA <lleida.guillem@gmail.com>
2019-12-15 22:36:01 +0100

    add de pizza pref + des boutons trop stylés

--- src/app/commander.service.ts
+++ src/app/commander.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pizza } from 'src/models/Pizza';
+import { PizzaFavoriteService } from './pizza-favorite.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommanderService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private pizzaFavorieService: PizzaFavoriteService) {}
   commanderPizza(pizza: Pizza) {
+    this.pizzaFavorieService.saveFavorite(pizza);
     return this.http.put('http://localhost:8000/commanderPizza', pizza);
   }
 }
--- src/app/favorite-pizza/favorite-pizza.component.ts
+++ src/app/favorite-pizza/favorite-pizza.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ɵɵload } from '@angular/core';
 import { Pizza } from 'src/models/Pizza';
 import { PizzaFavoriteService } from '../pizza-favorite.service';
 import { CommanderService } from '../commander.service';
+import { PizzaBufferService } from '../pizza-buffer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorite-pizza',
@@ -12,9 +14,16 @@ export class FavoritePizzaComponent implements OnInit {
 
 private pizza: Pizza;
 
-constructor(private pizzaFavorieService: PizzaFavoriteService, private commande: CommanderService ) { }
-s
+constructor(private pizzaFavorieService: PizzaFavoriteService, private commande: CommanderService, private pizzaBufferService : PizzaBufferService, private router : Router ) { }
+
   ngOnInit() {
+    this.pizza = this.pizzaFavorieService.loadFavorite();
+    
+  }
+
+  private commanderPizza() {
+    this.pizzaBufferService.setPizza(this.pizza);
+    this.router.navigate(['commande']);
   }
 
 }
